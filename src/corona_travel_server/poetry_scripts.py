a3a9e9d60563eb5328ee6c369a472b0e1a35ffa9
unrndm <unrndm@gmail.com>
2021-10-08 18:39:13 +0300

    added dev script

--- src/corona_travel_server/poetry_scripts.py
+++ src/corona_travel_server/poetry_scripts.py
@@ -11,3 +11,6 @@ def mypy():
 
 def black():
     call(["black", scr_path])
+
+def dev():
+    call(["uvicorn", "corona_travel_server.app:app", "--reload", "--reload-dir", scr_path, "--port", "4000"])
