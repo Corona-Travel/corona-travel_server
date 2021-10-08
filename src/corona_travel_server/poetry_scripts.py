d928f237dffd442f30460e4a104d8ee465db9f1f
unrndm <unrndm@gmail.com>
2021-10-08 18:28:45 +0300

    added black formatter

--- src/corona_travel_server/poetry_scripts.py
+++ src/corona_travel_server/poetry_scripts.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 from subprocess import call
 
+scr_path = Path(__file__) / ".."
+scr_path = scr_path.resolve()
+
 
 def mypy():
-    call(["mypy", Path(__file__)/".."])
+    call(["mypy", scr_path])
+
+
+def black():
+    call(["black", scr_path])
