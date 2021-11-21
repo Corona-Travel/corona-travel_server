596058a1f6d9b8146340932ebaa132ac55b8d1f5
helena2505 <elenakarelina833@gmail.com>
2021-11-21 16:33:50 +0300

    Feat db connection: handling more errors

--- src/corona_travel_server/db_connect.py
+++ src/corona_travel_server/db_connect.py
@@ -7,7 +7,7 @@ def get_db(url: AnyUrl):
     try:
         client = MongoClient(url, serverSelectionTimeoutMS = 5000)
         client.server_info()
-        if 'corona_travel5' not in client.list_database_names():
+        if 'corona_travel' not in client.list_database_names():
             print('Invalid database name')
             return None
         else:
